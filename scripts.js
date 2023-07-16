9d0c0d995503804a4344d755d485f095b7033ae8
Moodley <kyliemoodley323@gmail.com>
2023-07-16 12:33:37 +0200

    complete

---
+++ scripts.js
@@ -0,0 +1,69 @@
+/* 
+const salary = 4000;
+const lodging = 'apartment'
+const size = 'large'
+// Only change the syntax below (not the values or key names)
+const expenses = {
+    food: 51.7501,
+    transport:  10.2,
+} 
+const tax = {
+    734: '3%',
+    234: '20%',
+    913: '12%',
+    415: '38%',
+    502: '42%',
+}
+const rent = {
+    none: 0,
+    'small-room': 200,
+    'large-room': 300,
+    'small-apartment': 400,
+    'large-apartment': 800,
+    'small-house': 1200,
+    'large-house': 2400,
+}
+// You can change below however you want
+const taxAsDecimal = tax.913 / 100
+const startingAfterTax = salary * 1 - taxAsDecimal
+const type = lodging + size
+const balance = expenses(transport) - expenses(food) - expenses(rent.type) 
+console.log(balance)
+*/
+
+const salary = 4000;
+const lodging = "apartment";
+const size = "large";
+
+// Only change the syntax below (not the values or key names)
+
+const expenses = {
+  food: 51.7501,
+  transport: 10.2,
+};
+
+const tax = {
+  734: "3%",
+  234: "20%",
+  913: "12%", // use this
+  415: "38%",
+  502: "42%",
+};
+
+const rent = {
+  none: 0,
+  "small-room": 200,
+  "large-room": 300,
+  "small-apartment": 400,
+  "large-apartment": 800,
+  "small-house": 1200,
+  "large-house": 2400,
+};
+
+// You can change below however you want
+
+const taxAsDecimal = parseInt(tax[913]) / 100; // Remove . & add square brackets. Pareseint to change percentage into integer - 0.12
+const startingAfterTax = salary * (1 - taxAsDecimal); // Add brackets - 4000 * 0.88 = 3520
+const type = `${size}-${lodging}`; // Interpolation 800
+const balance = startingAfterTax - expenses.transport - expenses.food - rent[type];
+console.log(balance.toFixed(2));
